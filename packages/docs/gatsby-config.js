c73df5ea5144027a4e4f460b0eb896098ffc5be1
wangyi7099 <724003548@qq.com>
2019-08-04 13:04:41 +0800

    docs: add service worker

--- packages/docs/gatsby-config.js
+++ packages/docs/gatsby-config.js
@@ -1,4 +1,19 @@
 // gatsby-config.js
 module.exports = {
-  __experimentalThemes: ['antdsite']
+  __experimentalThemes: ['antdsite'],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `AntdSite`,
+        short_name: `AntdSite`,
+        start_url: `/`,
+        theme_color: '#002140',
+        background_color: '#001529',
+        display: `standalone`,
+        icon: `static/favicon.png`
+      }
+    },
+    'gatsby-plugin-offline'
+  ]
 };
