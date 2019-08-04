93f1649b76759f5bf20631ae6d79262340cd463b
wangyi7099 <724003548@qq.com>
2019-08-04 13:26:28 +0800

    Revert "docs: add service worker"
    This reverts commit c73df5ea5144027a4e4f460b0eb896098ffc5be1.

--- packages/docs/gatsby-config.js
+++ packages/docs/gatsby-config.js
@@ -1,19 +1,4 @@
 // gatsby-config.js
 module.exports = {
-  __experimentalThemes: ['antdsite'],
-  plugins: [
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `AntdSite`,
-        short_name: `AntdSite`,
-        start_url: `/`,
-        theme_color: '#002140',
-        background_color: '#001529',
-        display: `standalone`,
-        icon: `static/favicon.png`
-      }
-    },
-    'gatsby-plugin-offline'
-  ]
+  __experimentalThemes: ['antdsite']
 };
