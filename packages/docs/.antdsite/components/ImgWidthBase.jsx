b5531b5c9d2cfa5db5f8f0ca9c3a04b3f877e2c5
wangyi7099 <724003548@qq.com>
2019-08-10 14:55:05 +0800

    docs: fix component

--- packages/docs/.antdsite/components/ImgWidthBase.jsx
+++ packages/docs/.antdsite/components/ImgWidthBase.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { PageContext } from 'antdsite';
+import { resolvePathWithBase } from 'antdsite/src/default-theme/components/utils';
 
 export default ({ url, width }) => (
   <PageContext.Consumer>
     {ctx => {
-      return <img src={`${ctx.webConfig.base}/${url}`} width={width} />;
+      return <img src={resolvePathWithBase(url, ctx.webConfig.base)} width={width} />;
     }}
   </PageContext.Consumer>
 );
