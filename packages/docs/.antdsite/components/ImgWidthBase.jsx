9e4de088c1d77feba0f7be058aa35312f5755de1
wangyi7099 <724003548@qq.com>
2019-08-10 14:43:36 +0800

    docs: fix link

--- packages/docs/.antdsite/components/ImgWidthBase.jsx
+++ packages/docs/.antdsite/components/ImgWidthBase.jsx
@@ -4,7 +4,7 @@ import { PageContext } from 'antdsite';
 export default ({ url, width }) => (
   <PageContext.Consumer>
     {ctx => {
-      return <img src={`${ctx.webConfig.base}${url}`} width={width} />;
+      return <img src={`${ctx.webConfig.base}/${url}`} width={width} />;
     }}
   </PageContext.Consumer>
 );
