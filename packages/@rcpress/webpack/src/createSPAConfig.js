504e8e85b976498ed2b3933979da572ba9be98f0
wangyi7099 <724003548@qq.com>
2019-09-24 22:48:49 +0800

    update

--- packages/@rcpress/webpack/src/createSPAConfig.js
+++ packages/@rcpress/webpack/src/createSPAConfig.js
@@ -2,6 +2,7 @@ module.exports = function createSPAConfig(options, cliOptions, isProd, isServer)
   const fs = require('fs-extra');
   const path = require('path');
 
+  const LoadablePlugin = require('@loadable/webpack-plugin');
   const WebpackBar = require('webpackbar');
   const createBaseConfig = require('./createBaseConfig');
   const CopyPlugin = require('copy-webpack-plugin');
@@ -38,6 +39,15 @@ module.exports = function createSPAConfig(options, cliOptions, isProd, isServer)
         compiledIn: false
       }
     ]);
+  }
+
+  if (isServer) {
+    // loadable webpack plugin
+    config.plugin('LoadablePlugin').use(
+      new LoadablePlugin({
+        outputAsset: false
+      })
+    );
   }
 
   if (isProd) {
